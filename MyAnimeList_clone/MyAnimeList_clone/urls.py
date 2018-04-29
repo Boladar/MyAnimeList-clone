8c25ca02a85d0f32924ab4bfda00f6edb1d4843b
Unknown <aleksander.szamalek@gmail.com>
2018-04-29 15:28:27 +0200

    added base.html

--- MyAnimeList_clone/MyAnimeList_clone/urls.py
+++ MyAnimeList_clone/MyAnimeList_clone/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from . import views as mainViews
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',mainViews.IndexPage.as_view(),name='index'),
 ]
