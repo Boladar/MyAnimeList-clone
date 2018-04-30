a11d344765c66014846423677e84b5ed9dd18316
Unknown <aleksander.szamalek@gmail.com>
2018-04-30 18:03:26 +0200

    added basic css for list

--- MyAnimeList_clone/MyAnimeList_clone/settings.py
+++ MyAnimeList_clone/MyAnimeList_clone/settings.py
@@ -135,7 +135,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIR = [os.path.join(BASE_DIR,'static')]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static'),
+]
 
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
